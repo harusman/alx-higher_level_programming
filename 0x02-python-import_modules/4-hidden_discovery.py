7d1fd73d0ce89764838649f1be0ec601cc6a25b4
harusman <westamder32@gmail.com>
2022-09-25 13:54:32 +0000

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,14 @@
 # 4-hidden_discovery.py
 # Haruna Danladi Maina <westamder32@gmail.com>
 
-if __name__ == "__main__":
-    import hidden_4
-    defs = dir(hidden_4)
-    for i in range(0, len(defs)):
-        if defs[i][0] != '_' and defs[i][1] != '_':
-            print(defs[i])
+import hidden_4
+
+
+def decompile():
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
+            print(name)
+
+
+if (__name__ == '__main__'):
+    decompile()
