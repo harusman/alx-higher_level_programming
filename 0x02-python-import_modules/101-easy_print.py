4229b0ab99083b9bd79a515612bfe9551e232290
harusman <westamder32@gmail.com>
2022-09-27 12:42:45 +0000

    101

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-if idx < 0 or idx >= len(my_list):
-return None
-else:
-return my_list[idx]
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
