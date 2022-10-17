8ca0d6721b38d5bda2b2c1a8c413623e7da501fa
harusman <westamder32@gmail.com>
2022-10-17 07:34:29 +0000

    0x08. python

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Haruna Danladi Maina <westamder32@gmail.com>
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
