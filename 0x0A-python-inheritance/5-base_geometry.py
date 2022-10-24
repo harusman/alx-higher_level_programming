77afebe54c89e7f3e977a03841339b7b369db3fd
harusman <westamder32@gmail.com>
2022-10-24 07:57:37 +0000

    0x0A. python

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Haruna Danladi Maina <westamder32@gmail.com>
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+# Haruna Danladi Maina <westamder32@gmail.com>
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
